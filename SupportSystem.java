a6993ad67e206aa186c8b57c1242bbbb9017218e
Rifqy_Febryantoni <55282444+nupnupnupnup@users.noreply.github.com>
2024-10-14 21:34:54 +0700

    Update SupportSystem.java

--- SupportSystem.java
+++ SupportSystem.java
@@ -1,11 +1,10 @@
 import java.util.HashSet;
 
-
 public class SupportSystem
 {
     private InputReader reader;
     private Responder responder;
-    
+
     public SupportSystem()
     {
         reader = new InputReader();
@@ -20,28 +19,30 @@ public class SupportSystem
 
         while(!finished) {
             HashSet<String> input = reader.getInput();
+            System.out.println("User input: " + input);  
 
             if(input.contains("bye")) {
-                finished = true;
-            }
-            else {
+                System.out.println("Apakah Anda yakin ingin keluar? (ya/tidak)");
+                String confirmation = reader.getInput().iterator().next();
+                if (confirmation.equals("ya")) {
+                    finished = true;
+                }
+            } else {
                 String response = responder.generateResponse(input);
-                System.out.println(response);
+                System.out.println("Response: " + response);  
             }
         }
+
         printGoodbye();
     }
-
 
     private void printWelcome()
     {
         System.out.println("Selamat datang di Technical Support System.");
         System.out.println();
-        System.out.println("Apa masalah yang sedang anda hadapi?.");
-        System.out.println("Kami akan membantumu.");
+        System.out.println("Apa masalah yang sedang anda hadapi? Kami akan membantumu.");
         System.out.println("Ketik 'bye' jika ingin keluar.");
     }
-
 
     private void printGoodbye()
     {
